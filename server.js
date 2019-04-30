c21202fbc3d3702c6221714f7a29b9b6bdd35110
Levi Porter <levi@levibrooke.com>
2019-04-30 11:08:20 -0700

    Create weather endpoint and response

--- server.js
+++ server.js
@@ -12,5 +12,33 @@ app.get('/hello', (request, response) => {
   response.status(200).send('Hello');
 });
 
+// hold weather forecast
+const weatherForecast = [];
+
+// constructor for weather object
+var WeatherForecast = function(forecast, time) {
+  this.forecast = forecast;
+  this.time = time;
+  weatherForecast.push(this);
+};
+
+// weather endpoint
+app.get('/weather', (request, response) => {
+
+  // get lat long
+  const locationData = require('./data/geo.json');
+  let location = locationData.location;
+
+  // get weather data
+  const weatherData = require('./data/darksky.json');
+  weatherData.daily.data.forEach(el => {
+    new WeatherForecast(el.summary, el.time);
+  });
+
+  response
+    .status(200)
+    .send(weatherForecast);
+
+});
 
 app.listen(PORT,() => console.log(`Listening on port ${PORT}`));
